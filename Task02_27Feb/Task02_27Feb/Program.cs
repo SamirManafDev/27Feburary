a9a8f1618a4424f72b837027ce5c69f8daac7105
SamirManafDev <samirmanafli161@gmail.com>
2023-02-28 16:51:49 +0400

    Task 27 feburary

---
+++ Task02_27Feb/Task02_27Feb/Program.cs
@@ -0,0 +1,53 @@
+﻿//Task 27 feb
+
+
+//int num=128;
+//int qaliq;
+//while (num > 1)
+//{
+//    qaliq = num % 2;
+//    if (qaliq != 0)
+//    break;
+//    num /= 2;
+//}
+//    if (num == 1)
+//        Console.WriteLine("2nin quvvetidir");
+//    else
+//        Console.WriteLine("2nin quvveti deyil");
+
+
+
+
+
+
+
+
+
+//int num = 1234,num1,num2,num3,num4;
+//num1 = num / 1000;
+////Console.WriteLine(num1);
+//num2 = (num - (num1 * 1000))/100;
+////num2 = ((num - (num % 100)) % 1000)/100 ;
+////Console.WriteLine(num2);
+//num3= (num-(num1 * 1000)-(num2*100))/10;
+////Console.WriteLine(num3);
+//num4 = num % 10;
+////Console.WriteLine(num4);
+
+//if ((num1 > num2) && (num1 > num3) && (num1 > num3)) ;
+//{
+//    Console.WriteLine(num1);
+
+//    if ((num2 > num1) && (num2 > num3) && (num2 > num4)) ;
+//    {
+//        Console.WriteLine(num2);
+//    }
+//    if ((num3 > num1) && (num3 > num2) && (num3 > num4)) ;
+//    {
+//        Console.WriteLine(num3);
+//    }
+//    if ((num4 > num1) && (num4 > num2) && (num4 > num3)) ;
+//    {
+//        Console.WriteLine(num4);
+//    }
+//}
